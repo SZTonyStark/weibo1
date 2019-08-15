25b5eaecb13443b40d3017a5c61db9a7e0041d03
Tony <373886379@qq.com>
2019-08-15 03:34:51 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 class UsersController extends Controller
+
 {
     public function create()
     {
@@ -23,6 +24,13 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎，您将在这里开始一段新的旅程');
+        return redirect()->route('users.show',[$user]);
     }
 }
